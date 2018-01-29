7aa03bba152c944300f0e4533589bb041c4936ba
Sofia Malmsten <sofia.malmsten@burohappold.com>
2018-01-29 23:33:24 +0000

    Changed folders

---
+++ IES_Toolkit/CRUD/Create.cs
@@ -0,0 +1,33 @@
+﻿using BH.Adapter;
+using System.Collections.Generic;
+using BH.oM.Base;
+
+namespace BH.Adapter.IES
+{
+    public partial class IesAdapter : BHoMAdapter
+    {
+        /***************************************************/
+        /**** Protected Methods                         ****/
+        /***************************************************/
+
+        protected override bool Create<T>(IEnumerable<T> objects, bool replaceAll = false)
+        {
+            bool success = true;
+
+            if (typeof(IObject).IsAssignableFrom(typeof(T)))
+            {
+
+                foreach (T obj in objects)
+                {
+                    success &= Create(obj as dynamic);
+                }
+            }
+
+            return success;
+
+        }
+
+
+        /***************************************************/
+    }
+}
