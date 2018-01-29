2f9284c9e34147beec3bbba6fb03400322116460
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-01-29 17:27:39 +0000

    Set up folder structure and added references

---
+++ IES_Toolkit/IES_Engine/Convert/AdapterID.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using BHG = BH.oM.Geometry;
+using BHE = BH.oM.Environmental;
+
+
+namespace BH.Engine.IES
+{
+    public static partial class Convert
+    {
+        public const string AdapterID = "IES_id";
+    }
+}
+
---
+++ IES_Toolkit/IES_Engine/Convert/ToBHoM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BHE = BH.oM.Environmental;
+using BHS = BH.oM.Structural;
+using BHG = BH.oM.Geometry;
+using BHEE = BH.Engine.Environment;
+using BH.oM.Environmental.Properties;
+using BH.oM.Environmental.Elements;
+
+namespace BH.Engine.IES
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods - BHoM Objects             ****/
+        /***************************************************/
+
+        public static BHE.Elements.Building ToIES(this Building iesBuilding)
+        {
+            BHE.Elements.Building bHoMBuilding = new BHE.Elements.Building
+            {
+                //Latitude = tasBuilding.latitude,
+                //Longitude = tasBuilding.longitude,
+                //Elevation = tasBuilding.maxBuildingAltitude,
+
+            };
+            return bHoMBuilding;
+        }
+    }
+}
