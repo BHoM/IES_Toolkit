68b3fb2ab3bf054d03197af24faedd5a63168015
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-08-29 16:57:37 +0100

    Removing links to the Structural namespace

--- IES_Engine/Convert/ToBHoM.cs
+++ IES_Engine/Convert/ToBHoM.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using BHE = BH.oM.Environment;
-using BHS = BH.oM.Structural;
 using BHG = BH.oM.Geometry;
 using BH.oM.Environment.Properties;
 using BH.oM.Environment.Elements;
