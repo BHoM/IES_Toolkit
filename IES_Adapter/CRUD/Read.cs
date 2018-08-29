92022d28c224e4a50171cc0b2e99f6fb98d74121
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-08-29 09:11:40 +0100

    Cleaning up dependencies in the IES toolkit. Removing the link to the XML toolkit

--- IES_Adapter/CRUD/Read.cs
+++ IES_Adapter/CRUD/Read.cs
@@ -9,7 +9,6 @@ using BH.oM.Environment.Interface;
 using BHG = BH.oM.Geometry;
 using BH.Engine;
 using BHE = BH.oM.Environment;
-using XML_Adapter;
 
 namespace BH.Adapter.IES
 {
@@ -21,32 +20,12 @@ namespace BH.Adapter.IES
 
         protected override IEnumerable<IBHoMObject> Read(Type type, IList indices = null)
         {
-            if (type == typeof(Space))
-                return ReadSpaces();
-            if (type == typeof(BuildingElementPanel))
-                return ReadPanels();
-            return null;
+            throw new NotImplementedException();
         }
 
         /***************************************************/
         /**** Private Methods                           ****/
         /***************************************************/
-
-        private List<Space> ReadSpaces(List<string> ids = null)
-        {
-            BH.oM.XML.gbXML gbx = XMLReader.Load(Filepath, Filename);
-            IEnumerable<IBHoMObject> bHoMObject = gbXML.gbXMLDeserializer.Deserialize(gbx);
-            return bHoMObject.Where(x => x is BHE.Elements.Space).Cast<Space>().ToList();
-        }
-
-        /***************************************************/
-
-        private List<BuildingElementPanel> ReadPanels(List<string> ids = null)
-        {
-            BH.oM.XML.gbXML gbx = XMLReader.Load(Filepath, Filename);
-            IEnumerable<IBHoMObject> bHoMObject = gbXML.gbXMLDeserializer.Deserialize(gbx);
-            return bHoMObject.Where(x => x is BHE.Elements.BuildingElementPanel).Cast<BuildingElementPanel>().ToList();
-        }
     }
 }
 
