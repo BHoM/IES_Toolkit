80e984ba7d9d336ba8257608a079ae3cb45bfbaf
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-07-31 13:08:38 +0100

    Fix most opening pull issues #29

--- IES_Engine/Modify/RepairOpening.cs
+++ IES_Engine/Modify/RepairOpening.cs
@@ -8,6 +8,7 @@ using BH.oM.Geometry;
 using BH.oM.Environment.Elements;
 
 using BH.Engine.Environment;
+using BH.Engine.Geometry;
 
 using System.ComponentModel;
 using BH.oM.Reflection.Attributes;
@@ -16,8 +17,24 @@ namespace BH.Engine.IES
 {
     public static partial class Modify
     {
-        public static Opening RepairOpening(this Opening opening, Point panelXYZ, bool useXZ = false, bool useYZ = false)
+        //public static Opening RepairOpening(this Opening opening, Point panelXYZ, bool useXZ = false, bool useYZ = false)
+        public static Opening RepairOpening(this Opening opening, Panel host, List<Panel> panelsAsSpace)
         {
+            List<Point> vertices = host.Polyline().IDiscontinuityPoints();
+            bool useYZ = false;
+            bool useXZ = false;
+            if (vertices.Min(y => y.X) == vertices.Max(y => y.X))
+                useYZ = true;
+            else if (vertices.Min(y => y.Y) == vertices.Max(y => y.Y))
+                useXZ = true;
+
+            Point panelXYZ = host.BottomLeft(panelsAsSpace);
+            Point right = host.TopRight(panelsAsSpace);
+
+            bool increaseX = (panelXYZ.X <= right.X);
+            bool increaseY = (panelXYZ.Y <= right.Y);
+            bool increaseZ = (panelXYZ.Z <= right.Z);
+
             //The openings when converted from IES don't have the right coordinates for 3D space, this method will repair them
             Polyline pLine = opening.Polyline();
             List<Point> pts = pLine.ControlPoints;
@@ -25,24 +42,24 @@ namespace BH.Engine.IES
             List<Point> newPts = new List<Point>();
             foreach(Point p in pts)
             {
-                newPts.Add(new Point { X = p.X, Y = p.Y, Z = p.Z });
+                newPts.Add(new Point());
                 if (!useXZ && !useYZ)
                 {
-                    newPts.Last().X += panelXYZ.X;
-                    newPts.Last().Y += panelXYZ.Y;
+                    newPts.Last().X = (increaseX ? p.X + panelXYZ.X : Math.Abs(p.X - panelXYZ.X));
+                    newPts.Last().Y = (increaseY ? p.Y + panelXYZ.Y : Math.Abs(p.Y - panelXYZ.Y));
                     newPts.Last().Z = panelXYZ.Z;
                 }
                 else if (useXZ)
                 {
-                    newPts.Last().X += panelXYZ.X;
+                    newPts.Last().X = (increaseX ? p.X + panelXYZ.X : Math.Abs(p.X - panelXYZ.X));
                     newPts.Last().Y = panelXYZ.Y;
-                    newPts.Last().Z = p.Y + panelXYZ.Z;
+                    newPts.Last().Z = (increaseZ ? p.Y + panelXYZ.Z : Math.Abs(p.Y - panelXYZ.Z));
                 }
                 else if (useYZ)
                 {
                     newPts.Last().X = panelXYZ.X;
-                    newPts.Last().Y = p.X + panelXYZ.Y;
-                    newPts.Last().Z = p.Y + panelXYZ.Z;
+                    newPts.Last().Y = (increaseY ? p.X + panelXYZ.Y : Math.Abs(p.X - panelXYZ.Y));
+                    newPts.Last().Z = (increaseZ ? p.Y + panelXYZ.Z : Math.Abs(p.Y - panelXYZ.Z));
                 }
             }
 
---
+++ IES_Engine/Query/BottomRight.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Geometry;
+using BH.oM.Environment.Elements;
+using BH.Engine.Environment;
+using BH.Engine.Geometry;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.IES
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Gets the top right most point of a panel when looking from within the space to the outside")]
+        [Input("panel", "The Environment Panel to get the top right most point of")]
+        [Input("panelsAsSpace", "A collection of Environment Panels representing a single space")]
+        [Output("TopRightPoint", "The top right most point of the panel")]
+        public static Point TopRight(this Panel panel, List<Panel> panelsAsSpace)
+        {
+            Vector normal = panel.Polyline().Normal();
+            if (!panel.NormalAwayFromSpace(panelsAsSpace))
+                normal = panel.Polyline().Flip().Normal();
+
+            Point centre = panel.Polyline().Centroid();
+
+            Line line = new Line
+            {
+                Start = centre,
+                End = centre.Translate(normal),
+            };
+
+            List<Point> pnts = panel.Vertices();
+            double maxZ = pnts.Max(x => x.Z);
+            pnts = pnts.Where(x => x.Z == maxZ).ToList();
+
+            Point leftMost = null;
+            foreach (Point p in pnts)
+            {
+                if (!IsLeft(line, p))
+                    leftMost = p;
+            }
+
+            return leftMost;
+        }
+    }
+}
