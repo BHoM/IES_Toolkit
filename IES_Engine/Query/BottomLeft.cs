3cfa59b1fe91b978b1542652b44629e7521194dc
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-08-06 16:21:10 +0100

    #39 - fix issue with BottomLeft calculation

--- IES_Engine/Query/BottomLeft.cs
+++ IES_Engine/Query/BottomLeft.cs
@@ -42,15 +42,20 @@ namespace BH.Engine.IES
             double minZ = pnts.Min(x => Math.Round(x.Z, 6));
             pnts = pnts.Where(x => Math.Round(x.Z, 6) == minZ).ToList();
 
+            bool wasFlat = false;
+            TransformMatrix transform = null;
+
             if(pnts.Count == panel.Vertices().Count)
             {
                 //All the points are on the same Z level - we're looking at a floor/roof
                 Polyline pLine = panel.Polyline();
-                pLine = pLine.Rotate(pLine.Centroid(), new Vector { X = 1, Y = 0, Z = 0 }, 1.5708);
+                transform = BH.Engine.Geometry.Create.RotationMatrix(pLine.Centroid(), new Vector { X = 1, Y = 0, Z = 0 }, 1.5708);
+                pLine = pLine.Transform(transform);
                 pnts = pLine.ControlPoints;
                 minZ = pnts.Min(x => Math.Round(x.Z, 6));
                 pnts = pnts.Where(x => Math.Round(x.Z, 6) == minZ).ToList();
                 line.End = line.Start.Translate(pLine.Normal());
+                wasFlat = true;
             }
 
             Point leftMost = null;
@@ -59,6 +64,9 @@ namespace BH.Engine.IES
                 if (IsLeft(line, p))
                     leftMost = p;
             }
+
+            if (wasFlat && leftMost != null)
+                leftMost = leftMost.Transform(transform.Invert());
 
             return leftMost;
         }
