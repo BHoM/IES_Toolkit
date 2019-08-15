81adc81e7b9ee84eb305076a40a2c4db74dd2b02
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-08-15 17:17:07 +0100

    #45 update documentation

--- IES_Engine/Query/Polyline.cs
+++ IES_Engine/Query/Polyline.cs
@@ -1,29 +1,10 @@
-﻿/*
- * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
- *
- * Each contributor holds copyright over their respective contributions.
- * The project versioning (Git) records all such contribution source information.
- *                                           
- *                                                                              
- * The BHoM is free software: you can redistribute it and/or modify         
- * it under the terms of the GNU Lesser General Public License as published by  
- * the Free Software Foundation, either version 3.0 of the License, or          
- * (at your option) any later version.                                          
- *                                                                              
- * The BHoM is distributed in the hope that it will be useful,              
- * but WITHOUT ANY WARRANTY; without even the implied warranty of               
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
- * GNU Lesser General Public License for more details.                          
- *                                                                            
- * You should have received a copy of the GNU Lesser General Public License     
- * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
- */
-
-using BH.oM.Geometry;
+﻿using BH.oM.Geometry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
 
 namespace BH.Engine.IES
 {
@@ -33,6 +14,9 @@ namespace BH.Engine.IES
         /**** Public Methods - Bounding Box             ****/
         /***************************************************/
 
+        [Description("Get a 2d polyline representation of a BHoM BoundingBox")]
+        [Input("box", "The BHoM bounding box")]
+        [Output("polyline", "The 2d polyline representation of the box")]
         public static Polyline ToPolyline(this BoundingBox box)
         {
             Point min = box.Min.RoundedPoint();
