3a1de4ab326ed2789ca1f2faa2688f6cefd241bc
Maja Lindroth <maja.lindroth@burohappold.com>
2019-09-09 16:54:26 +0100

    #50 -  new object SettingsIES with PlanarTolerance

---
+++ IES_Engine/Create/SettingsIES.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.IES.Settings;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.IES
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Create a SettingsIES object for use with the IES Adapter")]
+        [Input("planarTolerance", "Set tolarance for planar surfaces")]     // need to come up with description 
+        [Output("settingsIES", "The IES settings to use with the IES adapter push")]
+        public static SettingsIES SettingsIES(double planarTolerance = BH.oM.Geometry.Tolerance.Distance)
+        {
+            return new SettingsIES
+            {
+                PlanarTolerance = planarTolerance,
+            };
+        }            
+    }
+}
---
+++ IES_oM/SettingsIES.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using BH.oM.Base;
+
+namespace BH.oM.IES.Settings
+{
+    public class SettingsIES : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public double PlanarTolerance { get; set; } = BH.oM.Geometry.Tolerance.Distance; 
+
+        /***************************************************/
+    }
+}
