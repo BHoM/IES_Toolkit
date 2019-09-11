08acba7f2f64b1254cfe80bd67bf3f4cca2f4ce4
Maja Lindroth <maja.lindroth@burohappold.com>
2019-09-11 17:09:40 +0100

    Fixed requested changes for PR

--- IES_Engine/Convert/Environment_oM/Opening.cs
+++ IES_Engine/Convert/Environment_oM/Opening.cs
@@ -22,6 +22,7 @@ namespace BH.Engine.IES
         [Input("opening", "The BHoM Environment Opening to convert")]
         [Input("panelsAsSpace", "The panels representing a single space which hosts this opening, used to check the orientation of the opening")]
         [Input("panelXYZ", "The bottom left corner point of the host panel to calculate the opening points from for GEM format")]
+        [Input("settingsIES", "The IES settings to use with the IES adapter")]
         [Output("iesOpening", "The string representation for IES GEM format")]
         public static List<string> ToIES(this Opening opening, List<Panel> panelsAsSpace, Point panelBottomRightReference, SettingsIES settings)
         {
@@ -132,6 +133,7 @@ namespace BH.Engine.IES
         [Description("Convert an IES string representation of an opening to a BHoM Environment Opening")]
         [Input("openingPts", "The string representations of coordinates that make up the opening")]
         [Input("openingType", "The IES representation of the opening type")]
+        [Input("settingsIES", "The IES settings to use with the IES adapter")]
         [Output("opening", "The BHoM Environment Opening converted from IES GEM format")]
         public static Opening ToBHoM(this List<string> openingPts, string openingType, SettingsIES settings)
         {
