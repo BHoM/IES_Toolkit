710f0adbc93f0472abc72c2da8beea4cd4eeb858
BHoMBot <BHoMBot@burohappold.com>
2020-04-07 15:23:36 +0100

    Make oM properties virtual

--- IES_oM/SettingsIES.cs
+++ IES_oM/SettingsIES.cs
@@ -35,10 +35,10 @@ namespace BH.oM.IES.Settings
         /***************************************************/
 
         [Description("Set tolarance for planar surfaces, default is set to BH.oM.Geometry.Tolerance.Distance")]
-        public double PlanarTolerance { get; set; } = BH.oM.Geometry.Tolerance.Distance;
+        public virtual double PlanarTolerance { get; set; } = BH.oM.Geometry.Tolerance.Distance;
 
         [Description("Set how many decimal places coordinates should have on export, default is set to 6")]
-        public int DecimalPlaces { get; set; } = 6;
+        public virtual int DecimalPlaces { get; set; } = 6;
 
         /***************************************************/
     }
