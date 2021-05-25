e0b7e46240569c9a6c5a048d3c99bec2bd139ff6
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2021-05-25 10:58:24 +0100

    Update IES_Engine/Convert/Environment/Space.cs

--- IES_Engine/Convert/Environment/Space.cs
+++ IES_Engine/Convert/Environment/Space.cs
@@ -86,7 +86,7 @@ namespace BH.Engine.Adapters.IES
                 List<Point> v = p.Vertices();
                 v.RemoveAt(v.Count - 1); //Remove the last point because we don't need duplicated points
 
-                if (!p.NormalAwayFromSpace(panels, settingsIES.PlanarTolerance)/* && p.ConnectedSpaces[0] == panels.ConnectedSpaceName()*/)
+                if (!p.NormalAwayFromSpace(panels, settingsIES.PlanarTolerance))
                     v.Reverse(); //Reverse the point order if the normal is not away from the space but the first adjacency is this space
 
                 string s = v.Count.ToString() + " ";
