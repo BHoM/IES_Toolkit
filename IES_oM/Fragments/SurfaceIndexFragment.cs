adad8910c1ab09ef9bbe9dc10dcf7fdd75d7ca5b
Karolina Prusicka <karolina.prusicka@burohappold.com>
2022-10-25 15:11:17 +0100

    Adding documentation

--- IES_oM/Fragments/SurfaceIndexFragment.cs
+++ IES_oM/Fragments/SurfaceIndexFragment.cs
@@ -11,6 +11,7 @@ namespace BH.oM.IES.Fragments
     [Description("Fragment containing the surface index from IES")]
     public class SurfaceIndexFragment : IFragment
     {
+        [Description("Index of panel in the space it is connected to")]
         public virtual int SurfaceID { get; set; } = -1;
 
     }
