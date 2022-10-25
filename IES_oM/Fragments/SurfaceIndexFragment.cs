1f21eec4aaa4afc665fac61165977605acdea480
Karolina Prusicka <karolina.prusicka@burohappold.com>
2022-10-25 11:52:10 +0100

    Add SurfaceID fragment

---
+++ IES_oM/Fragments/SurfaceIndexFragment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using System.ComponentModel;
+
+namespace BH.oM.IES.Fragments
+{
+    [Description("Fragment containing the surface index from IES")]
+    public class SurfaceIndexFragment : IFragment
+    {
+        public virtual int SurfaceID { get; set; } = -1;
+
+    }
+}
