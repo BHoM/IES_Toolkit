021bedab249a763838ab2adbcb2c7134667e1ba8
Karolina Prusicka <karolina.prusicka@burohappold.com>
2023-01-24 17:21:57 +0000

    Creating new methods

---
+++ IES_Adapter/Convert/Environment/Opening.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Environment.Elements;
+
+namespace BH.Adapter.IES
+{
+    public static partial class Convert
+    {
+        public static List<string> ToIES (this Opening opening)
+        {
+
+        }
+    }
+}
