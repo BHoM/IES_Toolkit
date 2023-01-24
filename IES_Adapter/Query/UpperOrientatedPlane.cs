472cc155c948403c74ce99ed8e607d0bb3e0334d
Karolina Prusicka <karolina.prusicka@burohappold.com>
2023-01-24 17:21:57 +0000

    Creating new methods

---
+++ IES_Adapter/Query/UpperOrientatedPlane.cs
@@ -0,0 +1,30 @@
+﻿using BH.Engine.Environment;
+using BH.Engine.Geometry;
+using BH.oM.Environment.Elements;
+using BH.oM.Geometry;
+using BH.oM.Geometry.CoordinateSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.IES
+{
+    public static partial class Query
+    {
+        public static Cartesian UpperOrientatedPlane(this Panel panel)
+        {
+            var plane = panel.Polyline().FitPlane();
+
+            if (plane.Normal.Z == 1 || plane.Normal.Z == -1)
+                return new Cartesian(plane.Origin, Vector.XAxis, Vector.YAxis, Vector.ZAxis);
+            else
+            {
+                var projY = new Vector() { X = 0, Y = 0, Z = 0 }.Project(plane.Normal);
+                var projX = projY.Rotate(Math.PI / -2, plane.Normal);
+                return new Cartesian(plane.Origin, projX, Vector.YAxis, Vector.ZAxis);
+            }
+        }
+    }
+}
---
+++ IES_Adapter/Query/XyzToXy.cs
@@ -0,0 +1,31 @@
+﻿using BH.Engine.Geometry;
+using BH.oM.Geometry;
+using BH.oM.Geometry.CoordinateSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.IES
+{
+    public static partial class Query
+    {
+        public static Point XyzToXy(this Point pt, Cartesian refPlane)
+        {
+            var diff = new Vector()
+            {
+                X = pt.X - refPlane.Origin.X,
+                Y = pt.Y - refPlane.Origin.Y,
+                Z = pt.Z - refPlane.Origin.Z,
+            };
+
+            return new Point()
+            {
+                X = refPlane.X.DotProduct(diff),
+                Y = refPlane.Y.DotProduct(diff),
+                Z = 0
+            };
+        }
+    }
+}
