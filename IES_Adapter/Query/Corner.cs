42339286e6f46db1b60fbfcecc78406369d8a43a
Karolina Prusicka <karolina.prusicka@burohappold.com>
2023-01-25 14:31:14 +0000

    Modifying new methods

--- IES_Adapter/Query/Corner.cs
+++ IES_Adapter/Query/Corner.cs
@@ -22,7 +22,20 @@ namespace BH.Adapter.IES
             
             var minX = coords.Min(pt => pt.X);
             var minY = coords.Min(pt => pt.Y);
+            return new Point() { X = minX, Y = minY, Z=0 }.XyToXyz(refPlane);
+        }
 
+        public static Point UpperRightCorner(this Panel panel)
+        {
+            var pline = panel.Polyline();
+            var refPlane = panel.UpperOrientatedPlane();
+
+            var coords = pline.ControlPoints;
+            coords = coords.Select(x => x.XyzToXy(refPlane)).ToList();
+
+            var maxX = coords.Max(pt => pt.X);
+            var maxY = coords.Max(pt => pt.Y);
+            return new Point() { X = maxX, Y = maxY, Z = 0 }.XyToXyz(refPlane);
         }
     }
 }
