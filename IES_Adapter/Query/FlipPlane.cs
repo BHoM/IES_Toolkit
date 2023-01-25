876118e83db17abe47a3a9ed8257471bd408da9e
Karolina Prusicka <karolina.prusicka@burohappold.com>
2023-01-25 14:31:14 +0000

    Modifying new methods

---
+++ IES_Adapter/Query/FlipPlane.cs
@@ -0,0 +1,25 @@
+﻿using BH.Engine.Geometry;
+using BH.oM.Geometry;
+using BH.oM.Geometry.CoordinateSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.IES
+{
+    public static partial class Query
+    {
+        public static Cartesian FlipPlane(this Cartesian refPlane)
+        {
+            return new Cartesian(
+                refPlane.Origin,
+                refPlane.X,
+                -refPlane.Y,
+                -refPlane.Z
+            );
+        }
+
+    }
+}
