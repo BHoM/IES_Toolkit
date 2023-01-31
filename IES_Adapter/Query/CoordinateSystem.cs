434e764eba7be45c24435b67e655aa10ebcc0605
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-01-31 10:19:41 +0000

    Update formatting

--- IES_Adapter/Query/CoordinateSystem.cs
+++ IES_Adapter/Query/CoordinateSystem.cs
@@ -36,20 +36,25 @@ namespace BH.Adapter.IES
     {
         public static Cartesian CoordinateSystem(this Polyline pLine, double distanceTolerance = Tolerance.MacroDistance, double angleTolerance = Tolerance.Angle)
         {
-            Vector locZ = pLine.Normal(distanceTolerance); Cartesian baseCartesiean; if (locZ.IsParallel(Vector.ZAxis, angleTolerance) != 0)
-            {
+            Vector locZ = pLine.Normal(distanceTolerance);
+            
+            Cartesian baseCartesiean;
+            
+            if (locZ.IsParallel(Vector.ZAxis, angleTolerance) != 0)
                 baseCartesiean = new Cartesian(pLine.ControlPoints[0], -Vector.XAxis, locZ.CrossProduct(-Vector.XAxis), locZ);
-            }
             else
             {
                 Vector locY = (Vector.ZAxis - Vector.ZAxis.DotProduct(locZ) * locZ).Normalise();
                 Vector locX = locY.CrossProduct(locZ);
                 baseCartesiean = new Cartesian(pLine.ControlPoints[0], locX, locY, locZ);
             }
+
             TransformMatrix locGlob1 = BH.Engine.Geometry.Create.OrientationMatrixLocalToGlobal(baseCartesiean);
             Point minPtLocal = pLine.ControlPoints.Select(x => x.Transform(locGlob1)).ToList().Bounds().Min;
             TransformMatrix globToLoc = BH.Engine.Geometry.Create.OrientationMatrixGlobalToLocal(baseCartesiean);
-            baseCartesiean.Origin = minPtLocal.Transform(globToLoc); return baseCartesiean;
+            baseCartesiean.Origin = minPtLocal.Transform(globToLoc);
+            
+            return baseCartesiean;
         }
     }
 }
